3a2126f1dd982411eb3ea0fbafe90a3b4becd7d7
studiomac-152 <studiomac-152@studiomac-152s-iMac.local>
2022-03-07 17:42:23 +0530

    updated and apply redux-thunk and apply container pattern

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -7,16 +7,27 @@ export const fetchCollectionsStart = () => ({
     type: ShopActionTypes.FETCH_COLLECTIONS_START,
 });
 
+export const fetchCollectionsSuccess = collectionsMap => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+    payload: collectionsMap
+})
+
+export const fetchCollectionsFailure = errorMessage => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: errorMessage
+})
+
+
 export const fetchCollectionsStartAsync = () => {
     return dispatch => {
         const collectionRef = firestore.collection('collections');
         dispatch(fetchCollectionsStart());
 
         collectionRef.get().then(snapshot => {
-            const collectionMap = convertCollectionsSnapshotToMap(snapshot);
-            
-            this.setState({ loding: false });
-        });
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            dispatch(fetchCollectionsSuccess(collectionsMap));
+        })
+        .catch(error => dispatch(fetchCollectionsFailure(error.message)));
     }
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,17 @@
-import {createStore, applyMiddleware} from 'redux';
+//Connect redux-think with redux project
+import {createStore, applyMiddleware} from 'redux'; //(1)import middleware
 import logger from 'redux-logger';
 import {persistStore} from 'redux-persist';
-import thunk from 'redux-thunk';
+import thunk from 'redux-thunk'; //(2)import thunk
 import rootReducer from './root-reducer';
 
-const middlewares = [thunk];
+const middlewares = [thunk]; 
 if (process.env.NODE_ENV === 'development'){
     middlewares.push(logger);
 }
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
-
+//(3)use applyMiddleware in create store function
+//(4)pass thunk in applyMiddleware function
 export const persistor = persistStore(store);
-
 
 export default {store, persistStore}; 
